3993fdfa8e5bee195b9ad664da45f2a773798f69
Akinlade Oluwadamilola <oluwadamilolaanuoluwapo@gmail.com>
2026-03-23 15:54:40 +0100

    Add products page with working cart and toast notifications

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 function openWhatsApp() {
 window.open("https://wa.me/07033416441", "_blank");
+=======
+function openWhatsApp() {
+window.open("https://wa.me/07033416441", "_blank");
+>>>>>>> 28d1e3b (Add products page with working cart and toast notifications)
 }
